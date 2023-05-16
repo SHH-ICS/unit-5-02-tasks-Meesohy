2d3c67f0a440ebeb0cc16f4e30fee84f5e775007
Meesoh <124698885+Meesohy@users.noreply.github.com>
2023-05-16 10:37:16 -0400

    Update Q1Length.py

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,8 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
+word= str()
+word= input ("Welcome! Enter a word:") 
+while word != "quit": 
+  print ("Length of word is", len(word), "letters long")
+  word= input("Enter another word:")
+print ("Goodbye! See you!") 
