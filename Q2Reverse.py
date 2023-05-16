8c19c0bd374f918682c20b5b59847b385a07c59f
Meesoh <124698885+Meesohy@users.noreply.github.com>
2023-05-16 10:38:35 -0400

    Update Q2Reverse.py

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,5 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+word= str()
+word= input("Welcome! Enter a word:")
+reversed= word[::-1]
+print(reversed)
